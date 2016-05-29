8cf5db3cc4e4e3aa891b5299ce9d9a350f830fa2
Cristian Molina <megatux@gmail.com>
2016-05-29 02:28:50 -0300

    Add Ohm Redis library

--- src/heroku_mini_app.cr
+++ src/heroku_mini_app.cr
@@ -1,8 +1,12 @@
 require "http/server"
 require "option_parser"
+require "ohm"
 
 bind = "0.0.0.0"
 port = 8080
+
+Ohm.redis = Resp.new ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379")
+Ohm.redis.call "SET", "app-name", "heroku_mini_app"
 
 OptionParser.parse! do |opts|
   opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
@@ -13,6 +17,7 @@ end
 server = HTTP::Server.new(bind, port) do |context|
   context.response.content_type = "text/plain"
   context.response << "Hello world, got #{context.request.path}"
+  context.response << "\nRedis get: #{Ohm.redis.call("GET", "app-name")}"
 end
 
 puts "Listening on http://#{bind}:#{port}"

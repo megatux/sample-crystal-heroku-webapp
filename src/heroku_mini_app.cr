abeeae9b8ebe19d970199eba6971b2f61fb08d62
Cristian Molina <megatux@gmail.com>
2016-05-29 00:15:30 -0300

    Add a mini HTTP service

--- src/heroku_mini_app.cr
+++ src/heroku_mini_app.cr
@@ -1,5 +1,19 @@
-require "./heroku_mini_app/*"
+require "http/server"
+require "option_parser"
 
-module HerokuMiniApp
-  # TODO Put your code here
+bind = "0.0.0.0"
+port = 8080
+
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
 end
+
+server = HTTP::Server.new(bind, port) do |context|
+  context.response.content_type = "text/plain"
+  context.response << "Hello world, got #{context.request.path}"
+end
+
+puts "Listening on http://#{bind}:#{port}"
+server.listen
